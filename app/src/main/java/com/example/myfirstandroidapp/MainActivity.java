1d0d646b2f73ed5d2c07216e21b6645fb1e6e324
Jonathan <jonathankim1228@hotmail.com>
2019-06-26 20:30:36 -0700

    init

---
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myfirstandroidapp;
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.myfirstandroidapp.R;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
